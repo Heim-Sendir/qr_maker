d7d6158f515a84df3b4193421d0e412808f91180
Heim-Sendir <alex.noa29@gmail.com>
2025-10-20 12:45:30 +0500

    add config file

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import os
+
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path(__file__).parents[1] / '.env'
+load_dotenv(env_path)
+
+BASE_URL = os.getenv('BASE_URL')
+CR_VALUE = os.getenv('CR_VALUE')
+OUTPUT_DIR = os.getenv('OUTPUT_DIR')
+QR_BOX_SIZE = int(os.getenv('QR_BOX_SIZE'))
+QR_BOX_SIZE_SMALL = int(os.getenv('QR_BOX_SIZE_SMALL'))
+QR_BORDER = int(os.getenv('QR_BORDER'))
+QR_BORDER_SMALL = int(os.getenv('QR_BORDER_SMALL'))
