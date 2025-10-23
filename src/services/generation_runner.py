81dd25ba86276c8aab565fc951346d4773bfff1a
Heim-Sendir <alex.noa29@gmail.com>
2025-10-23 11:32:33 +0500

    fix

--- src/services/generation_runner.py
+++ src/services/generation_runner.py
@@ -1,26 +1,38 @@
 
+import os
+
 from tqdm import tqdm
 from datetime import datetime
 
 dt = datetime.now().strftime('%d.%m.%y')
 
 class GenerationRunner:
-    def __init__(self, fm, report, qr):
+    def __init__(self, fm, report, qr, template=None, positions=None):
         self.fm = fm
         self.report = report
         self.qr = qr
-    
+        self.template = template
+        self.posotions = positions
+
     def run(self, merchants, mode: str):
         errors = 0
         report_count = 0
+        output_dir = self.fm.get_today_folder()
+
         desc_text = 'Генерация QR-кодов' if mode == 'qr' else 'Генерация QR с шаблоном'
 
         for m in tqdm(merchants, desc=desc_text, ncols=80):
             try:
                 if mode =='qr':
                     self.qr.generate_url(m)
-                else:
-                    self.qr.generate_url_with_template(m)
+
+                elif mode == 'template':
+                    qr_img = self.qr.generate_url_with_template(m)
+
+                    combined = self.template.place_qr(qr_img, self.posotions)
+                    output_path = os.path.join(output_dir, f'{m.name}_template.png')
+                    combined.save(output_path)
+                    m.qr_path = output_path
 
                 self.report.append({
                     'id': m.merchant_id,
@@ -29,9 +41,12 @@ class GenerationRunner:
                     'qr_path': getattr(m, 'qr_path', None),
                 })
                 report_count += 1
+
             except Exception as e:
                 errors += 1
-                print(f"❌ Ошибка при обработке {m.name}: {e}")
+                print(f"⚠️ Ошибка при обработке {m.name}: {e}")
+                continue
+
         self._print_summary(mode, report_count, errors)
 
     def _print_summary(self, mode, report_count, errors):
